25c5af68d4c4330fb5628d17642885ba6488c075
MuhammadSaid-Almunawar <159503330+MuhammadSaid-Almunawar@users.noreply.github.com>
2026-06-17 20:56:31 +0700

    Update challanges1.c

--- Modul1_BytesMemory/challanges1.c
+++ Modul1_BytesMemory/challanges1.c
@@ -1,1 +1,20 @@
+##1.	Buat fungsi xor_bytes(data: bytes, key: int) -> bytes yang meng-XOR setiap byte dalam data dengan key. 
+  #Test dengan data = b"Hello" dan key = 0x41. Verifikasi bahwa XOR dua kali menghasilkan data asli.
+def xor_bytes(data, key):
+    hasil = []
+    for b in data : 
+        hasil.append (b ^ key)
+    return bytes(hasil) # karena ada bytes = b')$--.'
 
+def decrypted(encrypted, key) :
+    hasil = []
+    for c in encrypted:
+        hasil.append(c ^ key)
+    return bytes(hasil)
+
+
+encrypted = xor_bytes(b"hello", 0x41)
+decrypted_data = decrypted(encrypted, 0x41 ) 
+
+print(encrypted)
+print(decrypted_data)
