c03f797700a9617a9a24621b057c9961c47a9c78
MuhammadSaid-Almunawar <159503330+MuhammadSaid-Almunawar@users.noreply.github.com>
2026-06-25 23:32:04 +0700

    Create challanges2.py

---
+++ Modul1_BytesMemory/challanges2.py
@@ -0,0 +1,2 @@
+C2
+Buat fungsi hexdump(data: bytes) yang menampilkan output seperti xxd: offset hex (4 digit), hex bytes (16 per baris), karakter printable (non-printable tampilkan titik). Test dengan b"Hello, World!\x00\xff".
